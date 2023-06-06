ee76aee486e1fbbc21feba9dd5dfc243962bc440
endlessfun1 <ibrahimsezer05@outlook.com>
2023-06-07 02:43:15 +0300

    Added Char Controller
    merged Char Prefab with Photon

--- TEAM-U63/Assets/İbrahim/Scripts/GameManager.cs
+++ TEAM-U63/Assets/İbrahim/Scripts/GameManager.cs
@@ -1,14 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
+using Unity.Mathematics;
 using UnityEngine.SceneManagement;
 
 namespace U63
 {
     public class GameManager : MonoBehaviourPunCallbacks
     {
+        #region Public Fields
+
+        [Tooltip("The prefab to use for representing the player")]
+        public GameObject playerPrefab;
+
+        #endregion
+        
         #region Photon Callbacks
 
         /// <summary>
@@ -55,6 +64,21 @@ namespace U63
         #endregion
 
         #region Private Methods
+
+        private void Start()
+        {
+            if (playerPrefab == null)
+            {
+                Debug.LogError("<Color=Red><a>Missing</a></Color> playerPrefab Reference. Please set it up in GameObject 'Game Manager'",this);
+            }
+            else
+            {
+                Debug.LogFormat("We are Instantiating LocalPlayer from {0}", Application.loadedLevelName);
+                // we're in a room. spawn a character for the local player. it gets synced by using PhotonNetwork.Instantiate
+                PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
+            }
+        }
+
 
         void LoadArena()
         {
