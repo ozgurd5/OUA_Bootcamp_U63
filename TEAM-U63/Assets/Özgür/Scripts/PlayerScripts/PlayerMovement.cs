ea38629937b0f82f7939c296730c22549ddea529
Özgür Dalbeler <ozgurdalbeler@gmail.com>
2023-06-09 01:54:38 +0300

    player prefab full host  + cube

--- TEAM-U63/Assets/Özgür/Scripts/PlayerScripts/PlayerMovement.cs
+++ TEAM-U63/Assets/Özgür/Scripts/PlayerScripts/PlayerMovement.cs
@@ -7,22 +7,21 @@ public class PlayerMovement : NetworkBehaviour
     
     private float horizontalMove;
     private float verticalMove;
-
+    
     private Rigidbody rb;
-
+    
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
-
+    
     private void Update()
     {
         if (!IsOwner) return;
         
         horizontalMove = Input.GetAxisRaw("Horizontal");
         verticalMove = Input.GetAxisRaw("Vertical");
-
+        
         rb.velocity = new Vector3(horizontalMove * speed, rb.velocity.y, verticalMove * speed);
-
     }
 }
