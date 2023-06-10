47b6d4c424e75d99a79d1e441112ac0ff254384b
Metin Batuhan Özücan <metbatuo@gmail.com>
2023-06-11 02:15:14 +0300

    input system basics

---
+++ TEAM-U63/Assets/Batu/InputSystem/InputManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    PlayerControls playerControls;
+
+    public Vector2 movementInput;
+    public float verticalInput;
+    public float horizontalInput;
+
+    private void OnEnable()
+    {
+        if (playerControls == null)
+        {
+            playerControls = new PlayerControls();
+
+            playerControls.PlayerMovement.Movement.performed += i => movementInput = i.ReadValue<Vector2>();
+        }
+
+        playerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
+
+    public void HandleAllInputs()
+    {
+        HandleMovementInput();
+        //HandleJumpingInput();
+        //HandleActionInput();
+    }
+
+    private void HandleMovementInput()
+    {
+        verticalInput = movementInput.y;
+        horizontalInput = movementInput.x;
+    }
+}
