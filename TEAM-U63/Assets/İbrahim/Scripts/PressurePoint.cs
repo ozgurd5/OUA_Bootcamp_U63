a9a2e31a03185917742157545a8e29ddc7a31c55
endlessfun1 <ibrahimsezer05@outlook.com>
2023-06-11 08:10:29 +0300

    updated pres. point mechanic
    updated script so that an animation can play when a puzzle piece is placed on pres. point

--- TEAM-U63/Assets/İbrahim/Scripts/PressurePoint.cs
+++ TEAM-U63/Assets/İbrahim/Scripts/PressurePoint.cs
@@ -6,16 +6,29 @@ using UnityEngine;
 public class PressurePoint : MonoBehaviour
 {
     public GameObject puzzlePiece;
+    public GameObject targetObject;
+    public string animationName;
+
+    private bool isPuzzlePlaced = false;
 
     private void OnTriggerStay(Collider other)
     {
-        if (puzzlePiece)
+        if (other.gameObject == puzzlePiece && !isPuzzlePlaced)
         {
-            Debug.Log( "staying");
-        }
-        else
-        {
-            Debug.Log( "left");
+            // Place the puzzle piece on the pressure point
+            //puzzlePiece.transform.position = transform.position;
+
+            // Enable the Animator component on the target object
+            Animator animator = targetObject.GetComponent<Animator>();
+            if (animator != null)
+            {
+                animator.enabled = true;
+                animator.Play(animationName);
+            }
+
+            // Set the puzzle as placed to prevent repeated triggering
+            isPuzzlePlaced = true;
         }
     }
 }
+
