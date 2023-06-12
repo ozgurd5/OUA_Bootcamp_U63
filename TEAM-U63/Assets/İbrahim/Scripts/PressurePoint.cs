8ffc96e71bfbcb3f33f02f5a91ca96f62150a6eb
endlessfun1 <ibrahimsezer05@outlook.com>
2023-06-12 07:56:38 +0300

    2nd update pres. point mechanic
    the elevator can now go up or down based on if a puzzle piece is on the pressure point

--- TEAM-U63/Assets/İbrahim/Scripts/PressurePoint.cs
+++ TEAM-U63/Assets/İbrahim/Scripts/PressurePoint.cs
@@ -9,6 +9,9 @@ public class PressurePoint : MonoBehaviour
     public GameObject targetObject;
     public string animationName;
 
+    private Animator animator;
+    //private Animation _animation;
+    
     private bool isPuzzlePlaced = false;
 
     private void OnTriggerStay(Collider other)
@@ -16,18 +19,55 @@ public class PressurePoint : MonoBehaviour
         if (other.gameObject == puzzlePiece && !isPuzzlePlaced)
         {
             // Place the puzzle piece on the pressure point
-            //puzzlePiece.transform.position = transform.position;
+            // puzzlePiece.transform.position = transform.position;
 
             // Enable the Animator component on the target object
-            Animator animator = targetObject.GetComponent<Animator>();
+            animator = targetObject.GetComponent<Animator>();
             if (animator != null)
             {
                 animator.enabled = true;
                 animator.Play(animationName);
-            }
 
-            // Set the puzzle as placed to prevent repeated triggering
-            isPuzzlePlaced = true;
+                // Set the bool parameter in the Animator controller
+                animator.SetBool("IsPuzzlePlaced", true);
+
+                // Set the puzzle as placed to prevent repeated triggering
+                isPuzzlePlaced = true;
+            }
+        }
+    }
+    
+    //private IEnumerator DisableAnimatorAfterDelay(float delay)
+    //{
+    //    yield return new WaitForSeconds(delay);
+//
+    //    //while (_animation.isPlaying.Equals(true))
+    //    //{
+    //    //    yield return null;
+    //    //}
+    //    
+//
+    //    // Disable the Animator component on the target object
+    //    animator.enabled = false;
+    //}
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == puzzlePiece)
+        {
+            // Reset the bool parameter in the Animator controller
+            animator.SetBool("IsPuzzlePlaced", false);
+            
+
+            //float currentAnimationDuration = animator.GetCurrentAnimatorStateInfo(0).length;
+            //
+//
+            //StartCoroutine(DisableAnimatorAfterDelay(currentAnimationDuration));
+            
+            
+
+            // Set the puzzle as not placed to allow triggering again
+            isPuzzlePlaced = false;
         }
     }
 }
