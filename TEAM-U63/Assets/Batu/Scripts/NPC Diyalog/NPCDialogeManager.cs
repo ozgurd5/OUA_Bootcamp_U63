9db818773af423f91d1f327f1c566024596c4a70
Özgür Dalbeler <ozgurdalbeler@gmail.com>
2023-07-18 01:03:10 +0300

    npc hardcore fix

---
+++ TEAM-U63/Assets/Batu/Scripts/NPC Diyalog/NPCDialogeManager.cs
@@ -0,0 +1,45 @@
+using System;
+using UnityEngine;
+
+public class NPCDialogeManager : MonoBehaviour
+{
+    [Header("Assign")]
+    [SerializeField] private GameObject objectToOpen;
+    
+    private Dialogue dialogue;
+    private Animator animator;
+
+    private void Start()
+    {
+        dialogue = objectToOpen.GetComponentInChildren<Dialogue>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (!col.CompareTag("Player")) return;
+
+        if (col.name == "ArtistPlayer")
+            col.transform.Find("ArtistCrosshairCanvas").Find("TalentIcons").gameObject.SetActive(false);
+        else
+            col.transform.Find("CoderCrosshairCanvas").Find("TalentIcons").gameObject.SetActive(false);
+        
+        objectToOpen.SetActive(true);
+        dialogue.StartDialogue();
+        animator.SetBool("isTalking", true);
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        if (!col.CompareTag("Player")) return;
+
+        if (col.name == "ArtistPlayer")
+            col.transform.Find("ArtistCrosshairCanvas").Find("TalentIcons").gameObject.SetActive(true);
+        else
+            col.transform.Find("CoderCrosshairCanvas").Find("TalentIcons").gameObject.SetActive(true);
+        
+        objectToOpen.SetActive(false);
+        dialogue.ResetDialogue();
+        animator.SetBool("isTalking", false);
+    }
+}
