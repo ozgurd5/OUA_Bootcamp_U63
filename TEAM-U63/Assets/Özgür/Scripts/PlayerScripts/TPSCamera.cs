331eee299fed4298abde4f5eaba8ff4aa99b861f
Özgür Dalbeler <ozgurdalbeler@gmail.com>
2023-08-26 02:54:27 +0300

    pause menu and player cameras added to scenes

--- TEAM-U63/Assets/Özgür/Scripts/PlayerScripts/TPSCamera.cs
+++ TEAM-U63/Assets/Özgür/Scripts/PlayerScripts/TPSCamera.cs
@@ -3,23 +3,41 @@ using UnityEngine;
 public class TPSCamera : MonoBehaviour
 {
     [Header("Assign")]
-    [SerializeField] private Transform followTargetTransform;
-    [SerializeField] private Transform lookAtTargetTransform;
     [SerializeField] private Vector3 offset = new Vector3(0f, 0f, -5f);
     
-    [SerializeField] private PlayerInputManager pim;
-    
+    private Transform followTargetTransform;
+    private Transform lookAtTargetTransform;
+    private PlayerInputManager pim;
+
     private Vector3 followTargetPreviousPosition;
     private Vector3 followTargetPositionDifference;
     
     private void Start()
     {
         //TODO: that wont work on robots, fix it
-        pim = followTargetTransform.GetComponent<PlayerInputManager>();
+        FindComponents();
         
         //Default values
         transform.position = followTargetTransform.position + offset;
         followTargetPreviousPosition = followTargetTransform.position;
+    }
+
+    //TODO: that wont work on robots, fix it
+    private void FindComponents()
+    {
+        if (name == "CoderCamera")
+        {
+            followTargetTransform = GameObject.Find("CoderPlayer").transform;
+            lookAtTargetTransform = followTargetTransform.Find("CoderCameraLookAt");
+        }
+
+        else
+        {
+            followTargetTransform = GameObject.Find("ArtistPlayer").transform;
+            lookAtTargetTransform = followTargetTransform.Find("ArtistCameraLookAt");
+        }
+        
+        pim = followTargetTransform.GetComponent<PlayerInputManager>();
     }
 
     private void Update()
